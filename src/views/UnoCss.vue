a42c320aee262e9d8cc2e1b66b524ff997540fc7
zuoxiaobai <guoqzuo@gmail.com>
2023-08-16 17:34:16 +0800

    feat: init

---
+++ src/views/UnoCss.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main">
+        unocss 测试
+        <div class="main-span">测试样式</div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <button class="btn-primary">测试</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less">
+.main-span {
+    color: red;
+    @apply bg-red-500 border-dashed border-spacing-3 text-white;
+}
+
+</style>
+
+<style>
+.btn-primary {
+    @apply py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75;
+}
+</style>
